87d2ab6a16d41b78b2750ea16fe437c2c4f3b929
anand pandit <atim4077@gmail.com>
2022-01-28 22:43:32 +0530

    about page added

---
+++ src/pages/Refund.tsx
@@ -0,0 +1,66 @@
+export default () => {
+  return (
+    <div className="px-6 py-6">
+      <h1 className="text-2xl text-gray-800 px-8">
+        Refund & Cancellation Policy
+      </h1>
+      <ol className="text-gray-700 list-decimal text-sm px-10">
+        <li>
+          For training programs offered on Xciteducation Trainings, except for
+          Internet of Things training and Job Oriented Specializations, a user
+          may request a refund anytime before their chosen batch commencement
+          date. A transaction processing charge of 20% of the training fee paid
+          by the user will be deducted and the remaining will be refunded within
+          15 working days (Monday to Friday, excluding national holidays) of the
+          refund request being raised by the user. However, once the user has
+          made the payment and accessed the training content, no refund would be
+          permitted. No refund will be provided for the Internet of Things
+          training or Job Oriented Specializations offered on Xciteducation
+          Trainings once the payment has been made. If a user is enrolled in a
+          training that has been withdrawn by Xciteducation Trainings, he/she
+          will be entitled to 100% refund within 15 working days (Monday to
+          Friday, excluding national holidays) of the training program being
+          withdrawn and the refund would be initiated by Xciteducation
+          Trainings.
+        </li>
+        <br />
+        <li>
+          For individual fresher job posts and premium internship upgrades, an
+          employer may request a 100% refund within 30 days of their purchase,
+          if they are not able to hire even 1 candidate. The entire amount will
+          be refunded within 15 working days (Monday to Friday, excluding
+          national holidays) of refund request being raised by the user. Please
+          note - Refund isn't available for subscription and bulk plans.
+        </li>
+        <br />
+        <li>
+          For advertisers who advertise their products, offerings, and services
+          on different Xciteducation Worldwide channels (Website, Newsletter,
+          Facebook page, other social media channels etc.), no
+          refund/cancellation would be permitted after the advertising campaign
+          has begun or the payment is received, whichever is earlier. If an
+          advertising campaign is cancelled by Xciteducation Worldwidedue to its
+          internal policies, the advertiser would be entitled to a refund on a
+          pro-rata basis (after deducting the amount for the deliverables
+          already served)
+        </li>
+        <br />
+        <li>
+          Not with-standing this policy, any specific terms and conditions
+          mentioned on individual product & service page, and/or as agreed with
+          a user before/during/after the transaction, will override the terms
+          and conditions mentioned here in case of a conflict.
+        </li>
+        <br />
+        <li>
+          We may make changes to our products & services, and associated terms &
+          conditions (including terms and conditions on refund and
+          cancellation), from time to time. You are advised to refer to our
+          latest terms and conditions on this page and associated
+          product/service specific terms and conditions page before making a
+          purchase.
+        </li>
+      </ol>
+    </div>
+  );
+};
